b152e2c580807e1915874a949bd983aeb2026687
mohammedpythons <mohammedshokai99@gmail.com>
2021-08-04 18:47:18 -0400

    fix the bug in using data from API

--- src/components/Application.js
+++ src/components/Application.js
@@ -69,6 +69,7 @@ const appointments = [
   }
 ];
 export default function Application(props) {
+  const [day, setDay] = useState('Monday');
 
   const [days, setDays] = useState([])
   useEffect(() => {
@@ -82,7 +83,7 @@ export default function Application(props) {
       <section className="sidebar">
         {<> <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
          <hr className="sidebar__separator sidebar--centered" />
-         <nav className="sidebar__menu"> <DayList days={days}  setDay={(event) => setDays([...days,event])} /></nav>
+         <nav className="sidebar__menu"> <DayList days={days} day={day} setDay={setDay} />/></nav>
          <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
