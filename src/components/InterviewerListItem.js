7a82bbe640a57eac41ca59c5f2e4475ed7be3a3f
mohammedpythons <mohammedshokai99@gmail.com>
2021-09-17 11:29:48 -0400

    fix pic layout

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,14 +5,14 @@ import 'components/InterviewerListItem.scss'
 
 export default function InterviewerListItem(props) {
     const {id, name, avatar, setInterviewer, selected} = props
+    console.log("this is my selected",selected)
 
 
-
-    let interViewClassName = classNames('interviewers__item', {'interviewers__item--selected': selected}, {'interviewers__item-image': avatar} )
+    let interViewClassName = classNames('interviewers__item', {'interviewers__item--selected': selected? avatar: null} )
     return (
         <li onClick={() => setInterviewer(id)} className={interViewClassName}>
             <img
-            className={interViewClassName}
+            className='interviewers__item-image'
             src={avatar}
             alt={name}
              />
